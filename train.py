5d8a9bbe12104f0077724fbdbdd7dd047ca09589
root <adamczyha>
2024-11-20 20:28:38 +0000

    RuntimeError: Expected all tensors to be on the same device, but found at least two devices, cuda:0 and cpu!

--- train.py
+++ train.py
@@ -48,51 +48,61 @@ torch.manual_seed(1337)
 if torch.cuda.is_available():
 	torch.cuda.manual_seed(1337)
 
-if config['training']['init_from'] == 'scratch':
-
-	train_config = config['training']
-	model_config = config['model']
-	batch_size = int(train_config['batch_size'])
-	mini_batch = int(train_config['mini_batch'])
-	block_size = int(model_config['block_size'])
-
-	try:
-		assert batch_size % (mini_batch * block_size * ddp_world_size) == 0
-	except AssertionError:
-		if master_process:
-			logger.critical(f'BATCH_SIZE is not divisible by mini_batch * block_size * world_size({mini_batch * block_size * ddp_world_size})')
-	grad_accum_steps = batch_size // (mini_batch * block_size * ddp_world_size)
-	if master_process:
-		logger.info(f'total desiered batch size {batch_size}')
-		logger.info(f'=> calculated in gradient accumation steps: {grad_accum_steps}')
-
-	model = GPT(config)
-	model.to(device)
-	if torch.cuda.is_available() and bool(train_config['compile']):
-		model.compile()
-
-	optimizer = model.configure_optimizer(weight_decay=0.1, learning_rate=0, device=device)
-	scheduler = CosineScheduler(optimizer, config) # does not depend on optimizer learnig rate
-
-elif config['training']['init_from'] == 'resume':
+resume_run = False
+if config['training']['init_from'] == 'resume':
+	resume_run = True
 	model_dir = Path(config['training']['path_to_resume_training'])
 	assert os.path.exists(model_dir)
 
 	checkpoint = torch.load(model_dir)
 	config['model'] = checkpoint['config']['model']
-	model = GPT(config).load_state_dict(checkpoint['model'])
-	optimizer = torch.optim.AdamW(list(model.named_parameters())).load_state_dict(checkpoint['optimizer'])
-	scheduler = CosineScheduler(optimizer, config).load_state_dict(checkpoint['scheduler'])
+	model = GPT(config)
+	model.load_state_dict(checkpoint['model'])
+	optimizer = model.configure_optimizer(weight_decay=0.1, learning_rate=0, device=device)
+	optimizer.load_state_dict(checkpoint['optimizer'])
+	scheduler = CosineScheduler(optimizer, config)
+	scheduler.load_state_dict(checkpoint['scheduler'])
+	logger.info(f'Continue traning. Starting on {checkpoint["step"]} step with loss {checkpoint["loss"]}')
 
 elif config['training']['init_from'] == 'gpt2':
 	model = GPT.from_pretrained('gpt2')
 	config[model] = model.config
 	optimizer = model.configure_optimizer(weight_decay=0.1, learning_rate=0, device=device)
 	scheduler = CosineScheduler(optimizer, config) 
-	
+	logger.info(f'GPT from pretrained loaded.')
+
+elif config['training']['init_from'] == 'scratch':
+
+	model = GPT(config)
+	optimizer = model.configure_optimizer(weight_decay=0.1, learning_rate=0, device=device)
+	scheduler = CosineScheduler(optimizer, config) # does not depend on optimizer learnig rate
+
+
+
+train_config = config['training']
+model_config = config['model']
+batch_size = int(train_config['batch_size'])
+mini_batch = int(train_config['mini_batch'])
+block_size = int(model_config['block_size'])
+
+
+try:
+	assert batch_size % (mini_batch * block_size * ddp_world_size) == 0
+except AssertionError:
+	if master_process:
+		logger.critical(f'BATCH_SIZE is not divisible by mini_batch * block_size * world_size({mini_batch * block_size * ddp_world_size})')
+grad_accum_steps = batch_size // (mini_batch * block_size * ddp_world_size)
+if master_process:
+	logger.info(f'total desiered batch size {batch_size}')
+	logger.info(f'=> calculated in gradient accumation steps: {grad_accum_steps}')
+
+model.to(device)
+if torch.cuda.is_available() and bool(train_config['compile']):
+	model.compile()
+
 dataset = TokenDataset(config=config, split='train', seed=0)
 sampler = ChankSampler(config=config, dataset=dataset, shuffle=True, seed=0)
-train_loader = DataLoader(dataset=dataset, batch_size=mini_batch * block_size,  sampler=sampler,  collate_fn=custom_collate_fn, pin_memory=True)
+train_loader = DataLoader(dataset=dataset, batch_size=(mini_batch *block_size),  sampler=sampler,  collate_fn=custom_collate_fn, pin_memory=True)
 
 if ddp:
 	model = DDP(model, device_ids=[ddp_local_rank])
@@ -101,17 +111,18 @@ step_per_epoch = len(dataset) // batch_size
 if master_process:
 	logger.info(f' {step_per_epoch} batches in epoch')
 
-epoch = -1
+epoch = -1 if 'checkpoint' not in locals() else checkpoint['epoch']   
 last_step = 0 if 'checkpoint' not in locals() else checkpoint['step'] + 1
 scaler = torch.amp.GradScaler(enabled=(device == 'cuda'))
 for step in range(last_step, int(config['optimizer']['max_steps'])):
 	t0 = time()
 	loss_accumulation = 0.0
 
-	if step % (step_per_epoch - 1) == 0:
+	if step % (step_per_epoch - 1) == 0 or resume_run:
 		epoch += 1
-		sampler.set_epoch(epoch)
+		sampler.set_epoch(epoch )
 		train_iter = iter(train_loader)
+		resume_run = False
 	ddpExist = model.no_sync() if ddp else nullcontext()
 	with ddpExist:
 		for micro_step in range(grad_accum_steps):
