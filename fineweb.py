a4cf80ce25bcd3f365638cadd4e8330afd99f6ad
root <marcel.kowalik.rekrutacja@gmail.com>
2025-03-05 22:48:40 +0000

    without fineweb midsave

--- fineweb.py
+++ fineweb.py
@@ -15,33 +15,26 @@ enc = tiktoken.get_encoding('gpt2')
 tokenizer = AutoTokenizer.from_pretrained("gpt2", use_fast=True)
 
 if __name__ == '__main__':
-	if not os.path.exists('only_text_fineweb'):
-		dataset = load_dataset('HuggingFaceFW/fineweb-2', name='pol_Latn', split='train', num_proc=num_proc, data_files = ['data/pol_Latn/train/000_00000.parquet', 'data/pol_Latn/train/000_00001.parquet', 'data/pol_Latn/train/000_00002.parquet', 'data/pol_Latn/train/000_00003.parquet', 'data/pol_Latn/train/000_00004.parquet', 'data/pol_Latn/train/000_00005.parquet'])
-		dataset = dataset.select_columns(['text'])
-		dataset.save_to_disk('only_text_fineweb')
-		shutil.rmtree('/root/.cache/huggingface/hub/datasets--HuggingFaceFW--fineweb-2/blobs')
-	else:
-		dataset = Dataset.load_from_disk('only_text_fineweb')
-
-	if not os.path.exists('split_dataset'):
-		def chunk_generator(dataset, max_chars=1000):
-			for example in dataset:
-				text = example["text"]
-				for i in range(0, len(text), max_chars):
-					yield {"text": text[i:i + max_chars]}
+	
+	dataset = load_dataset('HuggingFaceFW/fineweb-2', name='pol_Latn', split='train', num_proc=num_proc, data_files = ['data/pol_Latn/train/000_00000.parquet', 'data/pol_Latn/train/000_00001.parquet', 'data/pol_Latn/train/000_00002.parquet', 'data/pol_Latn/train/000_00003.parquet', 'data/pol_Latn/train/000_00004.parquet', 'data/pol_Latn/train/000_00005.parquet'])
+	dataset = dataset.select_columns(['text'])
+	shutil.rmtree('/root/.cache/huggingface/hub/datasets--HuggingFaceFW--fineweb-2/blobs')
+	
+	def chunk_generator(dataset, max_chars=1000):
+		for example in dataset:
+			text = example["text"]
+			for i in range(0, len(text), max_chars):
+				yield {"text": text[i:i + max_chars]}
 
 
-		chunked_dataset = Dataset.from_generator(chunk_generator, gen_kwargs={"dataset": dataset, "max_chars": tokenizer.max_len_single_sentence})
-		del dataset
-		split_dataset = chunked_dataset.train_test_split(test_size=0.0005, seed=0, shuffle=True, writer_batch_size=10000)
-		split_dataset['val'] = split_dataset.pop('test')
-		del chunked_dataset
-		gc.collect()
-		split_dataset.save_to_disk('split_fineweb')
-		shutil.rmtree('only_text_fineweb')
-		shutil.rmtree('/root/.cache/huggingface')
-	else:
-		split_dataset = DatasetDict.load_from_disk('split_fineweb')
+	chunked_dataset = Dataset.from_generator(chunk_generator, gen_kwargs={"dataset": dataset, "max_chars": tokenizer.max_len_single_sentence})
+	del dataset
+	split_dataset = chunked_dataset.train_test_split(test_size=0.0005, seed=0, shuffle=True, writer_batch_size=10000)
+	split_dataset['val'] = split_dataset.pop('test')
+	del chunked_dataset
+	gc.collect()
+	shutil.rmtree('/root/.cache/huggingface')
+	
 
 	def tokenize(example: dict[str, list[str]]) -> dict[str, object]:
 		# ids = enc.encode_ordinary(example['text'])
